e278da42541528d3e3d21addc2fa17829e496f29
Adrija Mukherjee <adrijamukherjee1997@gmail.com>
2020-04-04 23:14:01 +0530

    This is a class for a Bullet object. (#3)
    * added bullet object
    
    * added new line
    
    * changed comment

---
+++ src/GameObjects/Bullet.py
@@ -0,0 +1,21 @@
+import pygame
+
+from Interface.GameObjectInterface import GameObject
+
+class Bullet(GameObject):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        # change the hitbox size depending on the Image size of the bullet
+        self.hitbox = (self.x, self.y, 20, 20)
+
+    def drawCharacter(self, canvas):
+        pass
+
+    def propagate(self, step):
+        pass
+
+    
+
+    
