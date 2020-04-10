b1cb04bfc3f0d6b44ab62321f4536a92e4c21525
Sparsha Saha <saha.sparsha@gmail.com>
2020-04-10 16:37:57 +0530

    Enemy Logic added

--- src/GameObjects/Enemy.py
+++ src/GameObjects/Enemy.py
@@ -4,15 +4,18 @@ from GameObjects.Interface.GameObjectInterface import GameObject
 
 
 class Enemy(GameObject):
-    def __init__(self, x, y):
+    def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
-        # Change the dims of hitbox depending on Image size
-        self.hitbox = (self.x, self.y, 20, 20)
+        self.width = width
+        self.height = height
+        self.hitbox = (self.x, self.y, self.width, self.height)
+        self.image = pygame.image.load(
+            "GameObjects/SpriteImages/EnemySpaceship.png")
 
     def drawCharacter(self, canvas):
-        self.hitbox = (self.x, self.y, 20, 20)
-        pygame.draw.rect(canvas, (0, 0, 0), self.hitbox)
+        self.hitbox = (self.x, self.y, self.width, self.height)
+        canvas.blit(self.image, (self.x, self.y))
 
     def propagate(self, speed):
         self.y += speed
