f5e1240f1449f07cc8167ddd7e2acccba38d7d12
Sparsha Saha <saha.sparsha@gmail.com>
2020-04-11 15:36:42 +0530

    Enemy logic added

--- src/Game.py
+++ src/Game.py
@@ -1,5 +1,7 @@
 import pygame
 from pygame.locals import *
+import random
+
 from GameObjects.Player import Player
 from GameObjects.Enemy import Enemy
 
@@ -7,20 +9,50 @@ from GameObjects.Enemy import Enemy
 class Game:
     def __init__(self):
         self.flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.FULLSCREEN
-        self.screen = pygame.display.set_mode((600, 600), self.flags)
+
+        # Screen vairbales
+        self.screenHeight = 600
+        self.screenWidth = 600
+        self.screen = pygame.display.set_mode(
+            (self.screenWidth, self.screenHeight), self.flags)
         self.background = pygame.Surface(
             self.screen.get_size())
         self.background = self.background.convert()
         self.background.fill((250, 250, 250))
+
+        # Game-Environment variables
         self.isRunning = False
-        self.speed = 5
+        self.playerSpeed = 5
+        self.probabilityOfGeneration = 0.02
+
+        # Enemy Variables
+        self.aliveEnemies = []
+        self.enemyWidth = 20
+        self.enemyHeight = 17
+        self.enemySpeed = 3
+
+    def __generateEnemies__(self):
+        if len(self.aliveEnemies) < 3 or self.aliveEnemies[-1].y > (self.screenHeight + self.aliveEnemies[-1].height):
+            if random.uniform(0, 1) < self.probabilityOfGeneration:
+                self.aliveEnemies.insert(
+                    0, Enemy(random.randrange(self.enemyWidth, self.screenWidth - self.enemyWidth, 1), -10, self.enemyWidth, self.enemyHeight))
+
+    def __cleanEnemyIfDead__(self, enemy):
+        if enemy.y > self.screenHeight + enemy.height:
+            self.aliveEnemies.remove(enemy)
+
+    def __handleEnemies__(self):
+        self.__generateEnemies__()
+        for enemy in self.aliveEnemies:
+            enemy.propagate(self.enemySpeed)
+            self.__cleanEnemyIfDead__(enemy)
+            enemy.drawCharacter(self.screen)
 
     def runGame(self):
         pygame.init()
-        pygame.display.set_caption('Basic Pygame program')
+        pygame.display.set_caption('Shooting Game')
 
         player = Player(50, 560, 20, 20)
-        enemy = Enemy(100, 30, 20, 17)
         self.isRunning = True
 
         while self.isRunning:
@@ -30,15 +62,14 @@ class Game:
                 if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                     self.isRunning = False
 
-            if keys[pygame.K_LEFT] and player.x > self.speed:
-                player.propagate(-self.speed)
-            elif keys[pygame.K_RIGHT] and player.x < self.screen.get_width() - player.width - self.speed:
-                player.propagate(self.speed)
+            if keys[pygame.K_LEFT] and player.x > self.playerSpeed:
+                player.propagate(-self.playerSpeed)
+            elif keys[pygame.K_RIGHT] and player.x < self.screen.get_width() - player.width - self.playerSpeed:
+                player.propagate(self.playerSpeed)
 
             self.screen.blit(self.background, (0, 0))
             player.drawCharacter(self.screen)
-            enemy.drawCharacter(self.screen)
-            enemy.propagate(self.speed)
+            self.__handleEnemies__()
             pygame.display.flip()
 
         pygame.quit()
